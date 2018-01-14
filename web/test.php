ff3a73dba517fb1e4f12b35eac195517b12f2aab
keanedawg <cameronfife@hotmail.com>
2018-01-13 19:11:39 -0700

    adding log files

--- web/test.php
+++ web/test.php
@@ -1,5 +1,14 @@
 <?php
 
-  print "Hello World! This is php"
+  use Monolog\Logger;
+  use Monolog\Handler\StreamHandler;
 
+  print "Hello World! This is php";
+  // create a log channel
+  $log = new Logger('name');
+  $log->pushHandler(new StreamHandler('logfile.txt', Logger::WARNING));
+
+  // add records to the log
+  $log->warning('Foo');
+  $log->error('Bar');
 ?>
